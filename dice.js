496ceafa53c4323b67975e551001cc67750d9960
Henry Onu <onuhenry24@gmail.com>
2022-03-31 09:12:40 +0100

    Add condition for game over

--- dice.js
+++ dice.js
@@ -1,4 +1,4 @@
-var scores, activePlayer, roundScore;
+var scores, activePlayer, roundScore, gameOn;
 init();
 //Math.random returns a number less than 1
 //Math.floor rounds a float down
@@ -20,34 +20,42 @@ init();
 //only in that function it is call, it opposite is CALLBACK i.e outside
 document.querySelector(".btn-roll").addEventListener('click', function () {
     //random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    if (gameOn) {
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    //display result
-    var diceImage = document.querySelector('.dice');
-    diceImage.style.display = 'block';
-    diceImage.src = 'img/' + 'dice-' + dice + '.png';
+        //display result
+        var diceImage = document.querySelector('.dice');
+        diceImage.style.display = 'block';
+        diceImage.src = 'img/' + 'dice-' + dice + '.png';
 
-    if (dice > 1) {
-        roundScore += dice
-        document.getElementById("score-" + activePlayer).textContent = roundScore;
-    } else {
-        //Next Player
-         NextPlayer();
+        if (dice > 1) {
+            roundScore += dice
+            document.getElementById("score-" + activePlayer).textContent = roundScore;
+        } else {
+            //Next Player
+            NextPlayer();
+        }
     }
 });
 
 document.querySelector(".btn-hold").addEventListener('click', function () {
-    scores[activePlayer] += roundScore
-    document.querySelector("#total-" + activePlayer).textContent = scores[activePlayer];
-    if ((scores[activePlayer]) >= 10) {
+    if (gameOn) {
+      scores[activePlayer] += roundScore;
+      document.querySelector("#total-" + activePlayer).textContent =
+        scores[activePlayer];
+      if (scores[activePlayer] >= 10) {
         document.querySelector(".name-" + activePlayer).textContent =
-            "WINNER!!! 🏆";
-        document.querySelector(".player" + activePlayer).classList.remove('active');
-         document.querySelector(".player" + activePlayer).classList.add("winner");
-        document.querySelector(".dice").style.display = 'none';
-        document.querySelector("#total-" + activePlayer).style.color ='white';
-    } else {
+          "WINNER!!! 🏆";
+        document.querySelector(".player" + activePlayer).classList.remove("active");
+        document
+          .querySelector(".player" + activePlayer)
+          .classList.add("winner");
+        document.querySelector(".dice").style.display = "none";
+        document.querySelector("#total-" + activePlayer).style.color = "white";
+        gameOn = false;
+      } else {
         NextPlayer();
+      }
     }
     
 });
@@ -67,17 +75,20 @@ function init() {
     scores = [0, 0];
     activePlayer = 0;
     roundScore = 0;
+    gameOn = true;
     document.querySelector(".dice").style.display = "none";
 
     //getElementbyid() is faster and used for only id only
     document.getElementById("total-0").textContent = 0;
     document.getElementById("total-1").textContent = 0;
     document.getElementById("score-0").textContent = 0;
-    document.getElementById("score-0").textContent = 0;
+    document.getElementById("score-1").textContent = 0;
     document.querySelector(".name-0").textContent = "PLAYER 1";
     document.querySelector(".name-1").textContent = "PLAYER 2";
     document.querySelector(".player0" ).classList.add("active");
     document.querySelector(".player0").classList.remove("winner");
+    //when player 2 wins
+    document.querySelector(".player1").classList.remove("winner");
     document.querySelector("#total-" + activePlayer).style.color = "orangered";
     
 }
